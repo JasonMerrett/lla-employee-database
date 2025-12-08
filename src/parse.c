d500e386e0939e2ccb18748f742267bb35765654
Jason Merrett <jason.merrett@outlook.com>
2025-12-08 19:00:26 +0000

    update create db header

--- src/parse.c
+++ src/parse.c
@@ -10,7 +10,7 @@
 #include "common.h"
 #include "parse.h"
 
-int output_file(int fd, struct dbheader_t *dbhdr, struct employee_t *employees) {
+int output_file(int fd, struct dbheader_t *dbhdr) {
     if (fd < 0) {
         printf("Got a bad FD from the user\n");
         return STATUS_ERROR;
@@ -77,7 +77,7 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
     *headerOut = header;
 }
 
-int create_db_header(int fd, struct dbheader_t **headerOut) {
+int create_db_header(struct dbheader_t **headerOut) {
 	struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
     if (header == -1) {
         printf("Malloc failed to create db header\n");
